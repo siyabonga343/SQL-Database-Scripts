3bfe338c15e02736df65bdce38a276615ed1571d
siyabonga343 <siyabonga3phungula@gmail.com>
2025-03-22 01:17:09 +0200

    sales_databases.sql

---
+++ sales_databases.sql
@@ -0,0 +1,11 @@
+-- Create the Sales Database
+CREATE DATABASE SalesDB;
+
+-- Create and Drop a School Database
+CREATE DATABASE SchoolDB;
+
+-- Drop the 'SchoolDB' 
+DROP DATABASE SchoolDB;
+
+-- Commit message
+Added SQL queries for creating and dropping databases
